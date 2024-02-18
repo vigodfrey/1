989348f6cc9e2265c05df8910c828a45eeca5039
Victoria <vigodfrey@mail.ru>
2024-02-18 19:03:39 +0300

    smt

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//string[] array = {"Hello", "2", "world", ":-)"};
+//string[] array = {"1234", "1567", "-2", "computer science"};
+string[] array = {"Russia", "Denmark", "Kazan"};
+
+int string_length = 3;
+string[] result_array = new string[array.Length] ;
+int j=0;
+
+for(int i=0 ; i<array.Length; i++)
+   if(array[i].Length <= string_length)
+   {
+    result_array[j] = array[i];
+    j+=1;
+   }
+
+result_array = result_array.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+Console.WriteLine("[{0}]", string.Join(", ", result_array));
